840192cc8458343c11886bed11bd42acb2b15510
Bhargav Kokkula <bhargav.0194@gmail.com>
2026-07-09 16:43:00 +0530

    displays expiry and warning

--- CapstoneProjectFrontend/src/app/dtos/user-document.response.dto.ts
+++ CapstoneProjectFrontend/src/app/dtos/user-document.response.dto.ts
@@ -6,6 +6,7 @@ export interface UserDocumentResponseDto {
     description: string;
     documentStatus: string;
     createdAt: string;
+    expiryDate?: string;
 
     targetDepartmentId: number;
     targetDepartmentName: string;
